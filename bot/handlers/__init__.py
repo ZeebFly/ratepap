e6c4798782aa97a72cb3f23ad562ea449c372311
ZeebFly <zeebrobot@gmail.com>
2026-02-10 04:44:17 +0700

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,6 +1,7 @@
 from .start import router as start_router
 from .submit import router as submit_router
 from .admin import router as admin_router
+from .admin_settings import router as admin_settings_router
 
 def get_routers():
-    return [start_router, submit_router, admin_router]
+    return [start_router, submit_router, admin_router, admin_settings_router]
